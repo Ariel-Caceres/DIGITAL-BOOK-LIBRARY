eefef78dac89fa3f48b2e7bebd8109b1a03018dc
ariel <ariel10player@gmail.com>
2025-01-26 12:07:02 -0300

    si

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ let link = async () =>{
   let api = await fetch("https://raw.githubusercontent.com/Ariel-Caceres/DIGITAL-BOOK-LIBRARY/refs/heads/master/libros.json")
   let libros = await api.json();
   console.log(libros);
-  
-  cajDePortadas.innerHTML = "";
-  libros.library.forEach(l => {
-    cajDePortadas.innerHTML += `<div class="portada"><img src="${l.cover}" alt=""></div>`
-  });
+  while(cajDePortadas.length < 4){
+
+    cajDePortadas.innerHTML = "";
+    libros.library.forEach(l => {
+      cajDePortadas.innerHTML += `<div class="portada"><img src="${l.cover}" alt=""></div>`
+    });
+  }
 }
 
 link()
